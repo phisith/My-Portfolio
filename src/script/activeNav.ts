c7a3939ade352c995afd4d3e512eb73f51c68902
phisith <aham226@hotmail.com>
2022-11-05 16:48:24 +0700

    create about

---
+++ src/script/activeNav.ts
@@ -0,0 +1,25 @@
+// let sections = document.querySelectorAll("section");
+// let nav = document.querySelector("nav");
+// let navActives = nav?.querySelectorAll("[id]");
+// window.onscroll = () => {
+//   let current: any = "";
+
+//   sections.forEach((section) => {
+//     const sectionTop = section.offsetTop;
+//     console.log(sectionTop);
+//     current = section.getAttribute("id");
+//     // if (window.scrollY >= sectionTop - 60) {
+//     //   current = section.getAttribute("id");
+//     // }
+//   });
+
+//   navActives?.forEach((a) => {
+//     a.classList.remove("bg-white/20");
+//     if (a.id === current) {
+//       console.log("yes");
+//       a.classList.add("bg-white/20");
+//     }
+//   });
+// };
+
+export {};
